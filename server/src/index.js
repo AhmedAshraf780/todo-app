7b05e83ea0907c5732cc8a8c16aac9e8f0da47bb
miserable <amedashraf780@gmail.com>
2026-04-22 17:51:21 +0200

    ayhaga

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,21 @@
-console.log("how ya doing hema")
+import express from 'express';
+import cors from 'cors';
+
+
+const notes = []
+const todos = []
+const projects = []
+
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3175',
+}));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our todo-app');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+}); 
